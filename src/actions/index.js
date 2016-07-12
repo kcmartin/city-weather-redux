4bbf86b693f4a8aacfb248e9b15f15a85aa6c8f5
kcmartin <kcmartin@users.noreply.github.com>
2016-07-12 16:23:35 -0700

    Add OpenWeather API request

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { API_KEY } from '../keys';
+
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// single variable to hold aciton type
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
